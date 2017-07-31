c5e6a6484bd6448c2d43999448c6d007dd901f1b
yacent <540608491@qq.com>
2017-07-31 12:01:24 +0800

    mergeresadsa

--- src/pages/mypoint/mypoint.vue
+++ src/pages/mypoint/mypoint.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="mypoint">
      <headbar :url="'/main/my'" :title="'积分兑换'"></headbar>
-     <div class="ponit_static" >     
+     <div class="ponit_static" >
       <div><img :src="pics.point_icon" alt="" :style="{left:'19%'}">剩余积分：{{points}}分</div>
       <div><img :src="pics.tickets_icon" alt="" :style="{left:'19%'}">剩余夜宵券：{{tickets}}张</div>
     </div>
     <p><a href="#" class="gettic" v-on:click="getTicket"><img :src="pics.ticker" alt=""></a></p>
+    <confirm-status :iconName="name" :content="cont" v-if="show" @hideStatus="hideShow"></confirm-status>
   </div>
 </template>
 
@@ -13,6 +14,7 @@
 import headbar from '../../components/headbar'
 import confirm from '../../components/confirm'
 import {exchangeVoucher} from '../../api'
+import confirmStatus from '../../components/confirmStatus.vue'
 export default {
   name: 'mypoint',
   data () {
@@ -21,6 +23,9 @@ export default {
     return {
       points: points,
       tickets: tickets,
+      name: '',
+      cont: '',
+      show: false,
       pics: {
         ticker: require('../../assets/ic_food_ticket_card.jpg'),
         point_icon: require('../../assets/ic_food_score.png'),
@@ -36,10 +41,16 @@ export default {
             window.location = '/login.html'
           }
           var data = response.data
+          this.name = 'check'
+          this.cont = '成功'
+          this.show = true
           this.points = data.score
           this.tickets = data.voucher
         })
       }
+    },
+    hideShow () {
+      this.show = false
     }
   },
   created () {
@@ -54,7 +65,8 @@ export default {
   },
   components: {
     headbar,
-    confirm
+    confirm,
+    confirmStatus
   }
 }
 </script>
